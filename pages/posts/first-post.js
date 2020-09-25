ea0b3342817ee21ef6cd51fb8e081cf65fad8f7c
vishnath <vishnusnathan@gmail.com>
2020-09-25 19:35:57 +0530

    Layout Component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,11 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../../components/layout'
+
 export default function FirstPost() {
     return (
-        <>
+
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -14,6 +17,7 @@ export default function FirstPost() {
                     <a>Back to home</a>
                 </Link>
             </h2>
-        </>
+        </Layout>
+
     );
 }
